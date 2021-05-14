9ef3fbd214e0476da2969fd9c43cd81a6cd1dd88
theawesam-hub <84186958+theawesam-hub@users.noreply.github.com>
2021-05-14 15:27:52 +0200

    Add files via upload

---
+++ templates and libs/libs/web/Cookies.js
@@ -0,0 +1,34 @@
+class Cookies {
+	static setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+		var expires = "expires=" + d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+	static getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for (let i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return " ";
+	}
+	static checkCookie(cname) {
+		var cvalue = this.getCookie(cname);
+		if (cvalue != "") {
+			return cvalue;
+		} else {
+			cvalue = prompt("Please set " + cname + ":", "");
+			if (cvalue != "" && cvalue != null) {
+				this.setCookie(cname, cvalue, 365);
+				return cvalue;
+			}
+		}
+	}
+}
