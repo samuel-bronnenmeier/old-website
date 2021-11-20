4109d7f26f7dd84c5ab9b831a0a3898c1c86ed7d
theawesam <84186958+theawesam-hub@users.noreply.github.com>
2021-11-20 20:35:16 +0100

    Style and content changes to downloads/default

--- downloads/js/app.js
+++ downloads/js/app.js
@@ -7,9 +7,9 @@ var parameters = fullURL.substring(1);
 
 //Get the id
 var id = 0;
-id = parameters.substring(3);
+id = parameters.substring(3, 4);
 
-//--------------The actual app--------------
+//--------------Here all the magic happens--------------
 let content;
 
 function preload() {
@@ -18,14 +18,40 @@ function preload() {
 
 function setup() {
     noLoop();
+    noCanvas();
 }
 
 function draw() {
     if (content.version > 0) {
+        //----------Head----------
         document.getElementsByTagName("title")[0].innerHTML = "Downloads | " + content.title;
 
+        //----------Description----------
         document.getElementsByClassName("title")[0].innerHTML = content.title;
 
         document.getElementsByClassName("text")[0].innerHTML = content.descriptionText;
+
+        document.getElementsByClassName("thumbnail-img")[0].setAttribute("src", content.imgPaths[0]);
+
+        //----------Infobox----------
+        let tableRows = document.getElementsByClassName("info-table")[0].getElementsByTagName("tr");
+
+        tableRows[0].getElementsByTagName("td")[1].innerHTML = content.officialTitle;
+
+        tableRows[1].getElementsByTagName("td")[1].innerHTML = content.appVersion;
+
+        tableRows[2].getElementsByTagName("td")[1].innerHTML = content.operatingSystem;
+
+        tableRows[3].getElementsByTagName("td")[1].innerHTML = content.author;
+
+        tableRows[4].getElementsByTagName("td")[1].innerHTML = content.price;
+
+        //----------Download button----------
+        document.getElementById("download-btn").blur();
+
+        let downloadInstruction = document.getElementsByClassName("download-instruction")[0];
+
+        downloadInstruction.innerHTML = downloadInstruction.innerHTML.replace("[Title]", content.title);
+        downloadInstruction.innerHTML = downloadInstruction.innerHTML.replace("[OS]", content.operatingSystem);
     }
 }
