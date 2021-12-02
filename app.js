4b814472c98de9b2857d7ef5cd58abdf8e1fed7f
theawesam <84186958+theawesam-hub@users.noreply.github.com>
2021-12-02 19:43:09 +0100

    Make massive changes to home

--- app.js
+++ app.js
@@ -3,33 +3,41 @@ function getYOff(elem) {
 }
 
 function init() {
-    if ($(".title").width() + 20 > $(".title").height()) {
+    if ($(window).width() + 20 > $(window).height()) {
+
         //Size the title according to the device's height
         $(".title")
         .height($(window).height() - getYOff($(".title")[0]))
         .css("font-size", $(".title").height() / 3);
+
     } else {
-        //Size the title according to the device's height
+
+        //Size the title according to the device's width
         $(".title")
         .height($(window).height() - getYOff($(".title")[0]) + 100)
-        .css("font-size", $(".title").height() / 5);
+        .css("font-size", ($(".title").width() / 5) + "px");
+
     }
 
-    //Size the gradient under the title
-    $(".help-grad")
-    .height($(".title").height() / 3);
-
     //Size the under title
-    $(".under-title").css("font-size", ($(".title").height() / 7) + "px");
+    $(".under-title").css("font-size", ($(".title").height() / 9) + "px");
 }
 
 $(document).ready(function() {
     init();
 
-    //Fade in the title and under-title
-    $(".title").html("<span class='text'>I have the solution.</span>");
+    //Fade in the title
+    //$(".title").html("<span class='text'>I have the solution.</span>");
     $(".title .text").hide();
-    $(".title .text").fadeIn(2000);
+
+    //$(".under-title").html("<span class='text'>It just doesn't fit the problem...</span>");
+    $(".under-title .text").css("visibility", "hidden");
+
+    $(".title .text").fadeIn(2300, function() {
+
+        $(".under-title .text").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 2000);
+
+    });
 });
 
 $(window).resize(function () {
